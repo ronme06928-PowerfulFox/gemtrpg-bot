cd9c449c7413e9affef79b53bd1d10693b483563
RONME <ronme06928@gmail.com>
2026-01-24 04:17:00 +0900

    デプロイエラーの修正

--- render-start.sh
+++ render-start.sh
@@ -2,6 +2,9 @@
 # exit on error
 set -o errexit
 
+# 念のため依存関係を再確認・インストール
+pip install -r requirements.txt
+
 # デフォルト画像の登録・同期
 # 起動時（Start Command）なら環境変数が確実に読み込まれています
 echo "Registering default images..."
